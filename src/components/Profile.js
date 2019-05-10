4b1ed4fd72484d549cd7caa12403802dff0abcb0
Samson O'Shaughnessy <soshaughnessy300@gmail.com>
2019-05-10 14:29:12 -0400

    adds mark complete and delete todo button

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,33 +2,46 @@ import React, { Component } from 'react';
 import Todos from './Todos';
 
 export class Profile extends Component {
-    state = {
-        todos: [
-        {
-            id: 1,
-            title: 'Do the dishes',
-            completed: false
-        },
-        {
-            id: 2,
-            title: 'Clean room',
-            completed: false
-        },
-        {
-            id: 3,
-            title: 'Laundry',
-            completed: false
-        }
-        ]
+    constructor(props) {
+        super(props);
+        this.state = {
+            todos: [
+                {
+                    id: 1,
+                    title: 'Do the dishes',
+                    completed: false
+                },
+                {
+                    id: 2,
+                    title: 'Clean room',
+                    completed: false
+                },
+                {
+                    id: 3,
+                    title: 'Laundry',
+                    completed: false
+                }
+            ]
     }
-    markComplete = () => {
-        
+    }
+    markComplete = (id) => {
+        this.setState({ todos: this.state.todos.map(todo => {
+            if(todo.id === id) {
+                todo.completed = !todo.completed
+            }
+            return todo;
+        }) });
+    }
+
+    delTodo = (id) => {
+        this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] });
     }
 
     render() {
+        
         return (
         <div>
-            <Todos todos={this.state.todos} markComplete={this.markComplete}/>
+            <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
         </div>
         )
     }
